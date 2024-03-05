1ab855cd4ec5523b632dd94632b40249147a2866
Thomas Ribeiro <thomas18rs@gmail.com>
2024-03-05 17:29:53 -0300

    feat: updates exceptions

--- src/main/java/br/com/thomasribeiro/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/com/thomasribeiro/gestao_vagas/exceptions/UserFoundException.java
@@ -1,7 +1,12 @@
 package br.com.thomasribeiro.gestao_vagas.exceptions;
 
 public class UserFoundException extends RuntimeException {
+
     public UserFoundException() {
-        super("Usuário já existe");
+        super("User already exists");
+    }
+
+    public UserFoundException(String message) {
+        super(message);
     }
 }
